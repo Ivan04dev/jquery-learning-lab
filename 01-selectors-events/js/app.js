c9797be4ecc7247865f2f2735b90926f2105943c
iscid <iscidivandelgado@gmail.com>
2026-03-14 00:12:15 -0600

    Proyecto 01 - Selectores y eventos en jQuery - se agregan comentarios en app.js

--- 01-selectors-events/js/app.js
+++ 01-selectors-events/js/app.js
@@ -1,7 +1,10 @@
+// Similar a DOMContentLoaded
 $(document).ready(function () {
+  // Evento click en el botón
   $("#btnSaludar").click(function () {
+    // Lee y asigna el valor del input nombre
     let nombre = $("#nombre").val();
-
+    // Agrega al párrafo con id mensaje la cadena "Hola" y el nombre almacenado en la variable anterior
     $("#mensaje").text("Hola " + nombre);
   });
 });
